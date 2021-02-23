53822433d94031c35e1fb88cf4fba0469d944991
maxnyla <sariflor@yahoo.co.uk>
2021-02-23 19:07:28 +0000

    added registration page details and functionality as well as modifying database connectivity details

--- app.py
+++ app.py
@@ -27,6 +27,26 @@ def get_recipes():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("This username is already registered")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("User successfully registered")
+        return redirect(url_for("profile", username=session["user"]))
+
     return render_template("register.html")
 
 
