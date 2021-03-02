896a0ce09d79a9b6666697b3e20497d91c573883
maxnyla <sariflor@yahoo.co.uk>
2021-03-02 21:18:14 +0000

    Added search function

--- app.py
+++ app.py
@@ -25,6 +25,13 @@ def get_recipes():
     return render_template("recipes.html", recipes=recipes)
 
 
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    query = request.form.get("query")
+    recipes = list(mongo.db.recipes.find({"$text": {"$search": query}}))
+    return render_template("recipes.html", recipes=recipes)
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
