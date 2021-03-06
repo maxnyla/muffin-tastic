23810bd334feed065f3b8290a3fa87562253ca83
maxnyla <sariflor@yahoo.co.uk>
2021-03-06 19:59:38 +0000

    added validation to login in app.py

--- app.py
+++ app.py
@@ -106,33 +106,49 @@ def register():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    # Check if the message is POST
-    if request.method == "POST":
-        # Validate data in 
+    # Check if username exists in session variable
+    if session.get("username"):
+        # If so, redirect user to account page and flash message.
+        flash("You are already signed in as " +
+              session["user"])
+        return redirect(url_for(
+            "account", username=session["user"]))
 
+    # Else, check if method is POST
+    if request.method == "POST":
+        # Validate if the data provided is correct
+        if request.form.get("username") == "" or not validate_username(
+           request.form.get("username").lower()):
+            flash("Please enter a valid username")
+            return redirect(url_for("login"))
+        if request.form.get("password") == "" or not validate_password(
+           request.form.get("password")):
+            flash("Please enter a valid password")
+            return redirect(url_for("login"))
         # check if username exists in db
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
-
+        # if user exists in database:
         if existing_user:
             # ensure hashed password matches user input
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
+                    # flash success message
                     flash("Hi, {}!".format(
                         request.form.get("username")))
+                    # redirect to account
                     return redirect(url_for(
                         "account", username=session["user"]))
             else:
-                # invalid password match
+                # invalid password match:flash message and return to login page
                 flash("Incorrect Username and/or Password")
                 return redirect(url_for("login"))
-
+        # username doesn't exist in db: flash message and return to login page
         else:
-            # username doesn't exist
             flash("Incorrect Username and/or Password")
             return redirect(url_for("login"))
-
+    # if method is not POST: render signin.html template
     return render_template("login.html")
 
 
