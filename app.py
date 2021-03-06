455dcc943ed0d051ae20173a5432b9802ad41f12
maxnyla <sariflor@yahoo.co.uk>
2021-03-06 18:31:59 +0000

    fixed typo on username validation function

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ if os.path.exists("env.py"):
 # Validation functions
 
 
-def validate_name(username):
+def validate_username(username):
     # Validate username.
     # Allow letters, hyphens and underscores. No spaces.
     return re.match("^[a-zA-Z0-9-_]{5,15}$", username)
@@ -63,7 +63,7 @@ def search():
 def register():
     # Check if the message is POST
     if request.method == "POST":
-        # Validate data
+        # Validate data in username
         if request.form.get("username") == "" or not validate_username(
             request.form.get("username")):
             flash("Please enter a valid username")
@@ -79,7 +79,7 @@ def register():
 
         # If it does, flash message informing user that this username is already taken.
         if existing_user:
-            flash("This username is already registered")
+            flash("This username is already taken")
             return redirect(url_for("register"))
 
         # Insert new user registration into database with data provided
@@ -89,17 +89,27 @@ def register():
         }
         mongo.db.users.insert_one(register)
 
-        # put the new user into 'session' cookie
+        # put the new user into 'session' cookie and flash confirmation
         session["user"] = request.form.get("username").lower()
         flash("User successfully registered")
+        # redirect to account page
         return redirect(url_for("account", username=session["user"]))
-
+    
+    # If method is not POST, check if user exists in session variable
+    if session.get("username"):
+        # If so, redirect user to account page and display flash message
+        flash("You are already a registered user")
+        return redirect(url_for("account", username=session["user"]))
+    # Else, redirect to registration page
     return render_template("register.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    # Check if the message is POST
     if request.method == "POST":
+        # Validate data in 
+
         # check if username exists in db
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
